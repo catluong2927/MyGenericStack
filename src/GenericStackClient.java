61723e9b10ec30ed1a152b2335abef2e0bb8baf7
itachi2927 <nguyengiacatluong92@gmail.com>
2022-12-25 14:15:57 +0700

    MyGenericStack

---
+++ src/GenericStackClient.java
@@ -0,0 +1,10 @@
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+        System.out.println("\n 2. Stack of Strings");
+        MyGenericStack.stackOfIntegers();
+    }
+
+
+}
